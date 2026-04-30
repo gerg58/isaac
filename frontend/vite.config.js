728884e7d74e6ed7005ea7b22091a8fd69967505
greg <greg@newtonian.site>
2026-04-30 11:52:50 +1000

    Eliminate warnings in frontend build; Add 'rebuild' script to  package.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,6 +16,7 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         entryFileNames: `assets/[name]-[hash].js`,
